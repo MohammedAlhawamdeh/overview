c35ea81de830c9bca7bce48d3737f5539d2960a5
MohammedAlhawamdeh <hawamdeh9393@gmail.com>
2020-07-12 17:43:17 +0300

    wrapped them with functions

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict'
+
 var counter=0;
+
+function nameAndGender(){
 var name = prompt('Hey, what\'s your name?')
 alert('Welcome to our website mr ' + name)
 var gender = prompt('what is your gender?')
@@ -13,8 +16,10 @@ if (gender === 'male') {
     alert('welcome MSS ' + name)
     var female = 'MSS '
 }
+}
+nameAndGender()
 
-
+function question1(){
 var q1 = prompt('do you know me');
 
 while (q1.toLowerCase() !== 'yes' && q1.toLowerCase() !== 'y' && q1.toLowerCase() !== 'no' && q1.toLowerCase() !== 'n') {
@@ -27,7 +32,10 @@ if (q1.toLowerCase() === 'yes' || q1.toLowerCase() === 'y') {
 } else {
     alert('wrong answer')
 }
+}
+question1()
 
+function question2(){
 var q2 = prompt('do you love me?');
 
 while (q2.toLowerCase() !== 'yes' && q2.toLowerCase() !== 'y' && q2.toLowerCase() !== 'no' && q2.toLowerCase() !== 'n') {
@@ -40,7 +48,9 @@ if (q2.toLowerCase() === 'yes' || q2.toLowerCase() === 'y') {
 } else {
     alert('wrong answer')
 }
-
+}
+question2()
+function question3(){
 var q3 = prompt('can you hurt me?');
 
 while (q3.toLowerCase() !== 'yes' && q3.toLowerCase() !== 'y' && q3.toLowerCase() !== 'no' && q3.toLowerCase() !== 'n') {
@@ -53,7 +63,11 @@ if (q3.toLowerCase() === 'yes' || q3.toLowerCase() === 'y') {
 } else {
     alert('wrong answer')
 }
+}
+question3()
 
+
+function question4(){
 var q4 = prompt('do you kiss me');
 
 while (q4.toLowerCase() !== 'yes' && q4.toLowerCase() !== 'y' && q4.toLowerCase() !== 'no' && q4.toLowerCase() !== 'n') {
@@ -66,7 +80,11 @@ if (q4.toLowerCase() === 'yes' || q4.toLowerCase() === 'y') {
 } else {
     alert('wrong answer')
 }
+}
+question4()
 
+function question5(){
+    var gender;
 var q5 = prompt('do you hate me');
 
 while (q5.toLowerCase() !== 'yes' && q5.toLowerCase() !== 'y' && q5.toLowerCase() !== 'no' && q5.toLowerCase() !== 'n') {
@@ -79,17 +97,17 @@ if (q5.toLowerCase() === 'yes' || q5.toLowerCase() === 'y') {
 } else {
     alert('wrong answer')
 }
-
 if (gender === 'male') {
     alert('thank you so much for visiting our website ' + male + name)
 }
 if (gender === 'female') {
     alert('thank you so much for visiting our website ' + female + name)
 }
+}
+question5()
 
 
-
-
+function question6(){
 for (let i = 4; i > 0; i--) {
 
     var guess = prompt('Try to guess a number between 1 - 10? - you have only '+i+ ' tries')
@@ -117,10 +135,10 @@ for (let i = 4; i > 0; i--) {
     }
 
 }alert('the correct answer is '+correctAnswer)
+}
+question6()
 
-
-
-
+function question7(){
 var places = ['istanbul','izmir','trabzon','rize']
 
 for(let i=6;i>0;i--){
@@ -135,3 +153,5 @@ for(let i=6;i>0;i--){
 }
 alert('my fav places are '+places[0]+' ,'+places[1]+' ,'+places[2]+' and '+places[3])
 alert('your final score is '+counter +' out of 7')
+}
+question7()
